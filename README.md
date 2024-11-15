255689589a897c8e16064902e35d67ef16b4d51e
ytkimirti <yusuftaha9@gmail.com>
2024-11-15 14:09:58 +0300

    initialize repo

---
+++ README.md
@@ -0,0 +1,38 @@
+# React Redis ClI for Upstash Redis
+
+`@upstash/react-redis-cli` is a React component that provides a CLI interface to interact with Upstash Redis.
+
+<img src="public%2Fredis-cli.png" width="640px" />
+
+### Install
+
+Install the component via npm:
+
+```bash
+$ npm install @upstash/react-redis-cli
+```
+
+### Usage
+
+Here's a basic example of how to use the component:
+
+```tsx
+import { RedisCli } from "@upstash/react-redis-cli"
+
+import "@upstash/react-redis-cli/dist/index.css"
+
+export default function Page() {
+  return (
+    <main style={mainStyle}>
+      <RedisCli url={UPSTASH_REDIS_REST_URL} token={UPSTASH_REDIS_REST_TOKEN} />
+    </main>
+  )
+}
+
+const mainStyle = {
+  width: "100vw",
+  maxWidth: "900px",
+  height: "500px",
+  margin: "0 auto",
+}
+```
---
+++ src/playground.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import { RedisCli } from "./redis-cli"
+
+import "./cli.css"
+
+document.body.style.margin = "0"
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <main
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        background: "rgb(250,250,250)",
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          maxHeight: "32rem",
+          maxWidth: "48rem",
+          borderRadius: "0.5rem",
+          overflow: "hidden",
+        }}
+      >
+        <RedisCli
+          token={process.env.UPSTASH_REDIS_REST_TOKEN!}
+          url={process.env.UPSTASH_REDIS_REST_URL!}
+        />
+      </div>
+    </main>
+  </React.StrictMode>
+)
